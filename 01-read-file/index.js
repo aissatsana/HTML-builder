278a3e17b38aaae598ed4e7cebe0cfcc5974c351
Анастасия <anastasia@MacBook-Air-Anastasia.local>
2025-01-18 13:57:50 +0300

    fix: change names in 1 task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,4 +2,4 @@ const fs = require('fs');
 const path = require('path');
 const filePath = path.join(__dirname, 'text.txt');
 const readableStream = fs.createReadStream(filePath, { encoding: 'utf8' });
-readable.pipe(process.stdout);
+readableStream.pipe(process.stdout);
