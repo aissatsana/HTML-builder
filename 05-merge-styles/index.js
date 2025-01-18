6a09796e4090dc69cf26ad05e84906c26b90bac6
Анастасия <anastasia@MacBook-Air-Anastasia.local>
2025-01-18 15:21:34 +0300

    feat: solving 5 task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const styleFolder = path.join(__dirname, 'styles');
+const bundleFolder = path.join(__dirname, 'project-dist');
+const styles = [];
+
+async function bundleCss() {
+  try {
+    const cssFiles = await fs.promises.readdir(styleFolder);
+    for (const file of cssFiles) {
+      const filePath = path.join(styleFolder, file);
+      const stat = await fs.promises.stat(filePath);
+      if (stat.isFile() && path.extname(filePath) === '.css') {
+        const data = await fs.promises.readFile(filePath, 'utf8');
+        styles.push(data);
+      }
+    }
+    const bundleFile = path.join(bundleFolder, 'bundle.css');
+
+    try {
+      await fs.promises.stat(bundleFile);
+      await fs.promises.unlink(bundleFile);
+    } catch (e) {}
+
+    const writableStream = fs.createWriteStream(bundleFile);
+    for (const style of styles) {
+      writableStream.write(style + '\n');
+    }
+    writableStream.end();
+  } catch (e) {
+    console.error(e);
+  }
+}
+bundleCss();
